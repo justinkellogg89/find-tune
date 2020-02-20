176db194efe5b99e44991f191477c2bde15d7462
Sean Rooney <seandanielrooney@gmail.com>
2020-02-20 07:44:49 -0700

    more work on API call

--- assets/js/sean.js
+++ assets/js/sean.js
@@ -1,28 +1,27 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "giphy.p.rapidapi.com",
-		"x-rapidapi-key": "a14db8f8acmsh96bcdd8b84302d1p1c95dajsnfc135164e36b"
+
+
+$(document).ready(function(){
+	var searchName;
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://giphy.p.rapidapi.com/v1/gifs/search?q=",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "giphy.p.rapidapi.com",
+			"x-rapidapi-key": "a14db8f8acmsh96bcdd8b84302d1p1c95dajsnfc135164e36b"
+		}
 	}
-}
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+	$("#add-music").on("click", function(event) {
+		event.preventDefault();
+		$("#artist-gif").empty();
+		searchName = $("#music-input").val().trim();
+		settings.url = "https://giphy.p.rapidapi.com/v1/gifs/search?q=" + searchName;
 
-$("#add-gif").on("click", function(event) {
-    event.preventDefault();
-    console.log("click works")
-    searchName = $("#add-gif").val().trim();
-    console.log(searchName);
-    settings.url = "https://giphy.p.rapidapi.com/v1/gifs/search?q=" + searchName
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});}
 
-    $.ajax(settings).done(function(response) {
-        console.log(response);
-    });
 
-});
 
