b639c015b5509823e78cac1a426995ed8057cfac
JLKellogg <jlkellogg18@outlook.com>
2020-02-21 10:18:12 -0700

    fixed window title, implemented background behind individual returns

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,54 @@
 var searchName;
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        "x-rapidapi-key": "d2fbe879camshb3861a29657e700p10444djsnd41b07129f5d"
-    }
-}
-
+  async: true,
+  crossDomain: true,
+  url: "",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "d2fbe879camshb3861a29657e700p10444djsnd41b07129f5d"
+  }
+};
 
 // on click event for searching artists song
 $("#add-music").on("click", function(event) {
-    event.preventDefault();
-    //emptys div of past songs
-    $("#music-list").empty();
-    //grabs user input
-    searchName = $("#music-input").val().trim();
+  event.preventDefault();
+  //emptys div of past songs
+  $("#music-list").empty();
+  //grabs user input
+  searchName = $("#music-input")
+    .val()
+    .trim();
 
-    settings.url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + searchName
+  settings.url =
+    "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + searchName;
 
-    //ajax call to grab the songs
-    $.ajax(settings).done(function(response) {
-        console.log(response);
-        // for loop looping through array of songs grabbing title album and preview
-        for (let i = 0; i < 10; i++) {
+  //ajax call to grab the songs
+  $.ajax(settings).done(function(response) {
+    console.log(response);
+    // for loop looping through array of songs grabbing title album and preview
+    for (let i = 0; i < 10; i++) {
+      var songName = response.data[i].title;
+      var albumName = response.data[i].album.title;
+      var snippet = response.data[i].preview;
+      var audioElement = $("<audio controls>");
+      var sourceTag = $("<source>").attr("src", snippet);
+      sourceTag.attr("type", "audio/mpeg");
 
-            var songName = response.data[i].title;
-            var albumName = response.data[i].album.title;
-            var snippet = response.data[i].preview;
-            var audioElement = $("<audio controls>")
-            var sourceTag = $("<source>").attr("src", snippet);
-            sourceTag.attr("type", "audio/mpeg");
+      audioElement.append(sourceTag);
 
-
-            audioElement.append(sourceTag);
-
-            $("#music-list").append(
-
-                $("<div>")
-                .addClass(".test")
-                .append(
-                    $("<p>").text("Title: " + songName),
-                    $("<p>").text("album: " + albumName),
-                    audioElement
-                )
-
+      $("#music-list")
+        .show()
+        .append(
+          $("<div>")
+            .addClass("test")
+            .append(
+              $("<p>").text("Title: " + songName),
+              $("<p>").text("album: " + albumName),
+              audioElement
             )
-
-
-        }
-        $("music-input").empty();
-
-    });
-
+        );
+    }
+    $("music-input").empty();
+  });
 });
